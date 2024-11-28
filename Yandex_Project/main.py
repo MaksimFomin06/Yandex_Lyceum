d0eb28843e9bb8dff9cede158057270867714cfa
Maksim_Fomin <mr.maksim06@mail.ru>
2024-11-29 00:12:55 +0300

    The addition to the database is ready

--- Yandex_Project/main.py
+++ Yandex_Project/main.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QDateEdit, QTimeEdit
 from PyQt6.QtSql import QSqlDatabase, QSqlQuery
 
 
@@ -12,30 +12,51 @@ class EventManager(QMainWindow):
         self.initUi()
 
     def initUi(self):
+        self.label_error.hide()
+
         self.AddButton.clicked.connect(self.add_btn_click)
         self.replaceNameButton.clicked.connect(self.r_name_btn_click)
         self.replaceDateButton.clicked.connect(self.r_date_btn_click)
         self.replaceTimeButton.clicked.connect(self.r_time_btn_click)
         self.replaceGeoButton.clicked.connect(self.r_geo_btn_click)
         self.DeleteButton.clicked.connect(self.d_btn_click)
-    
+
+        self.db = QSqlDatabase.addDatabase("QSQLITE")
+        self.db.setDatabaseName("data/EventManager_db.sqlite")
+
     def add_btn_click(self):
+        self.db.open()
+        input_name = self.input_name.text()
+        input_date = self.input_date.date()
+        input_date = input_date.toString("dd.MM.yyyy")
+        input_time = self.input_time.time()
+        input_time = input_time.toString("hh:mm")
+        input_geo = self.input_geo.text()
         
+        if not all([input_name, input_date, input_time, input_geo]):
+            self.label_error.show()
+        else:
+            self.label_error.hide()
+            request = QSqlQuery(self.db)
+            query = f"INSERT INTO events VALUES('{input_name}', '{input_date}', '{input_time}', '{input_geo}')"
+            request.exec(query)
+        self.db.close()
+
 
     def r_name_btn_click(self):
-        
+        pass
 
     def r_date_btn_click(self):
-        
+        pass
 
     def r_time_btn_click(self):
-        
+        pass
 
     def r_geo_btn_click(self):
-        
+        pass
 
     def d_btn_click(self):
-        
+        pass
 
 
 
