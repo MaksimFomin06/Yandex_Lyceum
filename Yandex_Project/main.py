43909a1568c5f4a96cf0e848cdb98aa45f760258
Maksim_Fomin <mr.maksim06@mail.ru>
2024-11-29 01:25:21 +0300

    возврат к прежней версии

--- Yandex_Project/main.py
+++ Yandex_Project/main.py
@@ -1,4 +1,5 @@
 import sys
+
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QMainWindow, QDateEdit, QTimeEdit
 from PyQt6.QtSql import QSqlDatabase, QSqlQuery
@@ -8,65 +9,93 @@ class EventManager(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("design/design.ui", self)
-        self.init_ui()
+        self.initUi()
 
-    def init_ui(self):
+    def initUi(self):
         self.db = QSqlDatabase.addDatabase("QSQLITE")
         self.db.setDatabaseName("data/EventManager_db.sqlite")
 
-        self.fill_list()
+        self.fill_lst()
         self.label_error.hide()
 
         self.AddButton.clicked.connect(self.add_btn_click)
-        self.replaceNameButton.clicked.connect(lambda: self.update_field("event_name"))
-        self.replaceDateButton.clicked.connect(lambda: self.update_field("event_date"))
-        self.replaceTimeButton.clicked.connect(lambda: self.update_field("event_time"))
-        self.replaceGeoButton.clicked.connect(lambda: self.update_field("event_geo"))
-        self.DeleteButton.clicked.connect(self.delete_event)
+        self.replaceNameButton.clicked.connect(self.r_name_btn_click)
+        self.replaceDateButton.clicked.connect(self.r_date_btn_click)
+        self.replaceTimeButton.clicked.connect(self.r_time_btn_click)
+        self.replaceGeoButton.clicked.connect(self.r_geo_btn_click)
+        self.DeleteButton.clicked.connect(self.d_btn_click)
 
     def add_btn_click(self):
         self.db.open()
-        input_name = self.input_name.text().strip()
-        input_date = self.input_date.date().toString("dd.MM.yyyy")
-        input_time = self.input_time.time().toString("hh:mm")
-        input_geo = self.input_geo.text().strip()
-
+        input_name = self.input_name.text()
+        input_date = self.input_date.date()
+        input_date = input_date.toString("dd.MM.yyyy")
+        input_time = self.input_time.time()
+        input_time = input_time.toString("hh:mm")
+        input_geo = self.input_geo.text()
+        
         if not all([input_name, input_date, input_time, input_geo]):
             self.label_error.show()
         else:
             self.label_error.hide()
             request = QSqlQuery(self.db)
-            query = f"INSERT INTO events VALUES ('{input_name}', '{input_date}', '{input_time}', '{input_geo}')"
+            query = f"INSERT INTO events VALUES('{input_name}', '{input_date}', '{input_time}', '{input_geo}')"
             request.exec(query)
         self.db.close()
 
-    def update_field(self, field):
+    def r_name_btn_click(self):
         input_name = self.input_replace_box.currentText()
-        new_value = getattr(self, f"input_replace_{field}").text().strip()
-        if not new_value:
-            self.label_error.show()
-            return
-        self.label_error.hide()
+        replace_name = self.input_replace_name.text()
         self.db.open()
         request = QSqlQuery(self.db)
-        query = f"UPDATE events SET {field} = '{new_value}' WHERE event_name = '{input_name}'"
+        query = f"UPDATE events SET event_name = '{replace_name}' WHERE event_name = '{input_name}'"
         request.exec(query)
         self.db.close()
 
-    def delete_event(self):
+    def r_date_btn_click(self):
+        input_name = self.input_replace_box.currentText()
+        replace_date = self.input_replace_date.date()
+        replace_date = replace_date.toString("dd.MM.yyyy")
+        self.db.open()
+        request = QSqlQuery(self.db)
+        query = f"UPDATE events SET event_date = '{replace_date}' WHERE event_name = '{input_name}'"
+        request.exec(query)
+        self.db.close()
+
+    def r_time_btn_click(self):
+        input_name = self.input_replace_box.currentText()
+        replace_time = self.input_replace_time.time()
+        replace_time = replace_time.toString("hh:mm")
+        self.db.open()
+        request = QSqlQuery(self.db)
+        query = f"UPDATE events SET event_time = '{replace_time}' WHERE event_name = '{input_name}'"
+        request.exec(query)
+        self.db.close()
+
+    def r_geo_btn_click(self):
+        input_name = self.input_replace_box.currentText()
+        replace_geo = self.input_replace_geo.text()
+        self.db.open()
+        request = QSqlQuery(self.db)
+        query = f"UPDATE events SET event_geo = '{replace_geo}' WHERE event_name = '{input_name}'"
+        request.exec(query)
+        self.db.close()
+
+    def d_btn_click(self):
         input_name = self.input_delete_name.currentText()
         self.db.open()
         request = QSqlQuery(self.db)
-        query = f"DELETE FROM events WHERE event_name = '{input_name}'"
+        query = f"DELETE from events WHERE event_name = {input_name}"
         request.exec(query)
         self.db.close()
 
-    def fill_list(self):
+    def fill_lst(self):
         self.db.open()
         query = QSqlQuery("SELECT event_name FROM events")
         while query.next():
             self.input_replace_box.addItem(query.value(0))
             self.input_delete_name.addItem(query.value(0))
+
 
 
 if __name__ == "__main__":
