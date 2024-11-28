ebe1f6e2b89858582a9a3b7140976ee4c7d82e0f
Maksim_Fomin <mr.maksim06@mail.ru>
2024-11-28 23:13:07 +0300

    linking buttons

--- Yandex_Project/main.py
+++ Yandex_Project/main.py
@@ -9,6 +9,34 @@ class EventManager(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("design/design.ui", self)
+        self.initUi()
+
+    def initUi(self):
+        self.AddButton.clicked.connect(self.add_btn_click)
+        self.replaceNameButton.clicked.connect(self.r_name_btn_click)
+        self.replaceDateButton.clicked.connect(self.r_date_btn_click)
+        self.replaceTimeButton.clicked.connect(self.r_time_btn_click)
+        self.replaceGeoButton.clicked.connect(self.r_geo_btn_click)
+        self.DeleteButton.clicked.connect(self.d_btn_click)
+    
+    def add_btn_click(self):
+        
+
+    def r_name_btn_click(self):
+        
+
+    def r_date_btn_click(self):
+        
+
+    def r_time_btn_click(self):
+        
+
+    def r_geo_btn_click(self):
+        
+
+    def d_btn_click(self):
+        
+
 
 
 if __name__ == "__main__":
