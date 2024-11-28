672fe31f151d29dd7fa38ac9d0135e809ed295fe
Maksim_Fomin <mr.maksim06@mail.ru>
2024-11-28 23:03:42 +0300

    the basic layout has been made

--- Yandex_Project/main.py
+++ Yandex_Project/main.py
@@ -1,1 +1,18 @@
-print(123)
+import sys
+
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtSql import QSqlDatabase, QSqlQuery
+
+
+class EventManager(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("design/design.ui", self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = EventManager()
+    ex.show()
+    sys.exit(app.exec())
