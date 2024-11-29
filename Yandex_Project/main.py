4f7ea441c898f3171f8adb4c4a9a0572772716d0
Maksim_Fomin <mr.maksim06@mail.ru>
2024-11-29 18:59:25 +0300

    bug fixs

--- Yandex_Project/main.py
+++ Yandex_Project/main.py
@@ -1,7 +1,9 @@
+
 import sys
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow, QDateEdit, QTimeEdit
+from PyQt6.QtWidgets import QApplication, QMainWindow, QDateEdit, QTimeEdit, QTableWidgetItem, QHeaderView
 from PyQt6.QtSql import QSqlDatabase, QSqlQuery
+from PyQt6.QtCore import Qt
 
 
 class EventManager(QMainWindow):
@@ -16,6 +18,7 @@ class EventManager(QMainWindow):
         self.db.setDatabaseName("data/EventManager_db.sqlite")
 
         self.fill_list()
+        self.fill_db()
 
         self.AddButton.clicked.connect(self.add_btn_click)
         self.replaceNameButton.clicked.connect(lambda: self.update_field("event_name", "name"))
@@ -33,7 +36,35 @@ class EventManager(QMainWindow):
                                  "Запись удалена"]
     
     def fill_db(self):
-        pass
+        self.events_db.clearContents()
+        self.events_db.setRowCount(0)
+        self.db.open()
+        self.events_db.setColumnCount(4)
+        self.events_db.setHorizontalHeaderLabels(["Название", "Дата", "Время", "Место"])
+
+        query = QSqlQuery("SELECT event_name, event_date, event_time, event_geo FROM events")
+        row = 0
+        while query.next():
+            self.events_db.insertRow(row)
+            name = QTableWidgetItem(query.value(0))
+            date = QTableWidgetItem(query.value(1))
+            time = QTableWidgetItem(query.value(2))
+            geo = QTableWidgetItem(query.value(3))
+
+            for item in [name, date, time, geo]:
+                item.setTextAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
+                item.setFlags(item.flags() ^ Qt.ItemFlag.ItemIsEditable)
+
+            self.events_db.setItem(row, 0, name)
+            self.events_db.setItem(row, 1, date)
+            self.events_db.setItem(row, 2, time)
+            self.events_db.setItem(row, 3, geo)
+
+            self.events_db.resizeRowsToContents()
+            header = self.events_db.horizontalHeader()
+            header.setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents.Stretch)
+            
+            row += 1
         
     def add_btn_click(self):
         self.db.open()
@@ -57,18 +88,25 @@ class EventManager(QMainWindow):
         self.db.close()
         self.update()
         
-
     def update_field(self, field, param):
         input_name = self.input_replace_box.currentText()
+        request = QSqlQuery(self.db)
+        self.db.open()
+        
         if not input_name:
             self.label_replace_notification.setText(self.replace_div_texts[0])
         else:
             new_value = getattr(self, f"input_replace_{param}").text().strip()
-            self.db.open()
-            request = QSqlQuery(self.db)
-            query = f"UPDATE events SET {field} = '{new_value}' WHERE event_name = '{input_name}'"
+            if not new_value:
+                self.label_replace_notification.setText(self.replace_div_texts[0])
+            query = f"SELECT event_name FROM events WHERE event_name = '{new_value}'"
             request.exec(query)
-            self.label_replace_notification.setText(self.replace_div_texts[1])
+            if request.first():
+                self.label_replace_notification.setText(self.add_div_texts[1])
+            else:
+                query = f"UPDATE events SET {field} = '{new_value}' WHERE event_name = '{input_name}'"
+                request.exec(query)
+                self.label_replace_notification.setText(self.replace_div_texts[1])
         self.db.close()
         self.update()
 
@@ -93,6 +131,7 @@ class EventManager(QMainWindow):
             self.input_delete_name.addItem(query.value(0))
 
     def update(self):
+        self.fill_db()
         self.input_replace_box.clear()
         self.input_delete_name.clear()
         self.db.open()
