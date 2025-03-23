13927b27d7c8eb091343b9e46db5bc1be069d6f1
Maksim_Fomin <mr.maksim06@mail.ru>
2025-03-23 15:08:38 +0300

    Сделал второе  задание

---
+++ flask1/classwork2.py
@@ -0,0 +1,37 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def mission_name():
+    return "Миссия Колонизация Марса"
+
+@app.route("/index")
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+@app.route("/promotion")
+def promotion():
+    return f"""<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>task 2</title>
+</head>
+
+<body>
+    <p>Человечество вырастает из детства.</p>
+    <p>Человечеству мала одна планета.</p>
+    <p>Мы сделаем обитаемыми безжизненные пока планеты.</p>
+    <p>И начнем с Марса!</p>
+    <p>Присоединяйся!</p>
+</body>
+
+</html>"""
+
+
+if __name__ == "__main__":
+    app.run(port=8080, host="127.0.0.1")
