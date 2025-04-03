9dce40391ce411c033164ba5aed2fc65096ade16
Maksim_Fomin <mr.maksim06@mail.ru>
2025-04-03 20:39:07 +0300

    добавил в приложение обработчик страницы с профессиями

--- flask-wtf/main.py
+++ flask-wtf/main.py
@@ -14,5 +14,17 @@ def index(substitution=None):
     return render_template("index.html", **context)
 
 
+@app.route("/list_prof/<list_type>")
+def prof_list(list_type):
+    prof_lst = ("инженер-исследователь", "пилот", "строитель", "экзобиолог", "врач", "инженер по терраформироаванию", "климатолог", "специалист по радиационной защите", "астрогеолог", "гляциолог", "инженер жизнеобеспечения", "метеоролог", "оператор марсохода", "киберинженер", "штурман", "пилот дронов")
+    context = {
+        "professions": prof_lst,
+        "list_type": list_type
+    }
+    
+    return render_template("list_prof.html", **context)
+
+
+
 if __name__ == "__main__":
     app.run(port=8000, host='127.0.0.1')
