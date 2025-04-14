cfac04feeadd5c9f52729acd18a9126415b83694
Maksim_Fomin <mr.maksim06@mail.ru>
2025-04-14 15:05:33 +0300

    добавил новую связь в бд

--- Flask-sqlalchemy 2/mars one/data/users.py
+++ Flask-sqlalchemy 2/mars one/data/users.py
@@ -20,6 +20,8 @@ class User(SqlAlchemyBase, UserMixin):
     hashed_password = sqlalchemy.Column(sqlalchemy.String)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
 
+    jobs = sqlalchemy.orm.relationship("Jobs", back_populates="user")
+
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
 
