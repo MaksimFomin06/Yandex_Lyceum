734dd9a7041854d7da78836aac6c969bc4b99148
Maksim_Fomin <mr.maksim06@mail.ru>
2025-05-01 00:49:01 +0300

    написал api blueprint

---
+++ web_rest-api/mars/mars_api.py
@@ -0,0 +1,26 @@
+from flask import make_response, jsonify, Blueprint, request
+
+from data import db_session
+from data.jobs import Jobs
+
+
+blueprint = Blueprint(
+    "jobs_api", 
+    __name__,
+    template_folder='templates'
+)
+
+
+@blueprint.route("/api/jobs")
+def get_jobs():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    return jsonify(
+        {
+            'jobs':
+                [item.to_dict(only=('team_leader', 'job', 'work_size', 'collaborators', 'start_date', 'end_date', 'is_finished'))
+                 for item in jobs]
+        }
+    )
+
+
