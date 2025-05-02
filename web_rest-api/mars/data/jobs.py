c0a11d9e8f81e0f8fb7484f93a8617d3c08e5833
Maksim_Fomin <mr.maksim06@mail.ru>
2025-05-02 13:08:25 +0300

    добавил новые параметры для некоторых строк

--- web_rest-api/mars/data/jobs.py
+++ web_rest-api/mars/data/jobs.py
@@ -11,12 +11,12 @@ class Jobs(SqlAlchemyBase, SerializerMixin):
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     team_leader = sqlalchemy.Column(
-        sqlalchemy.Integer, 
+        sqlalchemy.Integer,
         sqlalchemy.ForeignKey('users.id')
     )
-    job = sqlalchemy.Column(sqlalchemy.String)
-    work_size = sqlalchemy.Column(sqlalchemy.Integer)
-    collaborators = sqlalchemy.Column(sqlalchemy.String)
+    job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     start_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.now)
     end_date = sqlalchemy.Column(sqlalchemy.DateTime)
     is_finished = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
